f2c19f0cfbbe88fca867340e528391b287a0e202
srilq <git@srilq.email>
2017-09-10 14:05:31 +0100

    .

--- test.js
+++ test.js
@@ -5,11 +5,11 @@ const path = require('path')
 const press = require('.')
 const letter = require('./test.md')
 
-const date = new Date()
 const data = JSON.parse(fs.readFileSync(path.join(__dirname, 'test.json')))
+const date = new Date()
+const dateString = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
 data.from.forEach(from => data.to.forEach(to => {
-  const today = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
-  const md = letter(today, from, to)
+  const md = letter(dateString, from, to)
   const id = from.id + '_' + to.id
   press(id, md)
 }))
