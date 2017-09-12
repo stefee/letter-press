3dd2b1675d8a62d51073b9aae0808c19850bdc95
srilq <git@srilq.email>
2017-09-12 23:04:13 +0100

    rewrite

--- index.js
+++ index.js
@@ -1,83 +1,134 @@
 const fs = require('fs')
 const path = require('path')
+const extend = require('xtend')
 const ghmd = require('./ghmd')
 const puppeteer = require('puppeteer')
 
-module.exports = (id, markdown, opts) => {
-  const o = {}
-  o.quiet = opts && opts.quiet ? opts.quiet : false
-  o.dist = opts && opts.dist ? opts.dist : path.resolve('dist')
-  o.ghmd = opts && opts.markdown ? opts.markdown : null
-  o.pdf = opts && opts.pdf ? opts.pdf : {
-    format: 'A4'
-  }
-
-  if (typeof id !== 'string') return Promise.reject(new Error('Error: String argument not supplied: id'))
-  if (typeof markdown !== 'string') return Promise.reject(new Error('Error: String argument not supplied: markdown'))
-  if (!fs.existsSync(o.dist)) fs.mkdirSync(o.dist)
-  if (!fs.existsSync(o.dist)) return Promise.reject(new Error('Error: dist directory does not exist and could not be created: ' + o.dist))
-
-  return press(id, markdown, o).catch(e => console.error('🚨 ' + e))
-}
-
-async function press (id, markdown, opts) {
-  const pathMd = path.join(opts.dist, id + '.md')
-  const pathHtml = path.join(opts.dist, id + '.html')
-  const pathPdf = path.join(opts.dist, id + '.pdf')
-
-  try {
-    // write md
-    write(pathMd, markdown)
-        .then(file => opts.quiet || console.log('✨ ' + id + '.md done'))
-        .catch(e => console.error('🚨 ' + e))
-
-    // write html
-    const html = await ghmd(id, markdown, opts.ghmd)
-        .catch(e => console.error('🚨 ' + e))
-    if (!html) throw new Error('Something went wrong while generating HTML.')
-    await write(pathHtml, html)
-        .then(file => opts.quiet || console.log('✨ ' + id + '.html done'))
-        .catch(e => console.error('🚨 ' + e))
-
-    // write pdf
-    if (!fs.existsSync(pathHtml)) throw new Error('Something went wrong while generating HTML.')
-    await pdf(pathHtml, pathPdf, opts.pdf)
-        .then(file => opts.quiet || console.log('✨ ' + id + '.pdf done'))
-        .catch(e => console.error('🚨 ' + e))
-  } catch (e) {
-    throw e
-  }
-}
-
-async function pdf (file, out, opts) {
-  opts.path = out
-  let browser
-  try {
-    browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto('file://' + file, {
-      waitUntil: 'networkidle'
-    })
-    await page.pdf(opts)
-    browser.close()
-    return out
-  } catch (e) {
-    if (browser && browser.close) {
-      console.error('🚨 Error: Problem printing PDF. Closing browser...')
-      browser.close()
+module.exports = {
+  launch: (opts) => {
+    return new Press(opts).launch()
+  },
+  print: async (id, markdown, opts) => {
+    const press = new Press(opts)
+    try {
+      await press.launch()
+      await press.print(id, markdown)
+      press.close()
+    } catch (e) {
+      if (press && press.close) press.close()
+      throw e
     }
-    throw e
-  }
+  },
+  Press: Press
 }
 
-function write (file, data) {
+function writeFile (file, data) {
   return new Promise((resolve, reject) => {
     fs.writeFile(file, data, err => {
       if (err) {
         reject(err.toString())
         return
       }
-      resolve(file)
+      resolve(true)
     })
   })
+}
+
+function Press (opts) {
+  this._opts = extend({
+    path: path.resolve('dist'),
+    writeMarkdown: true,
+    pdf: {
+      format: 'A4'
+    }
+  }, opts || {})
+  this._browser = null
+  this._page = null
+  this._printJobs = []
+}
+
+Press.prototype.launch = async function () {
+  if (this._browser && this._browser.close) return this
+
+  try {
+    this._browser = await puppeteer.launch()
+    return this
+  } catch (e) {
+    console.error('🚨  Unexpected error while launching browser!')
+    throw e
+  }
+}
+
+Press.prototype.close = function () {
+  if (this._browser && this._browser.close) {
+    try {
+      this._browser.close()
+      delete this._browser
+    } catch (e) {
+      console.error('🚨  Unexpected error while closing browser!')
+      throw e
+    }
+  }
+}
+
+Press.prototype.print = async function (id, markdown, opts) {
+  const o = extend(this._opts, opts || {})
+
+  try {
+    if (!this._browser || !this._browser.close) throw new Error('Press not initialised. Press.prototype.launch is required first.')
+    if (typeof id !== 'string') throw new Error('Expected String for first argument: id')
+    if (typeof markdown !== 'string') throw new Error('Expected String for second argument: markdown')
+    if (!fs.existsSync(o.path)) {
+      try {
+        fs.mkdirSync(o.path)
+      } catch (e) {
+        throw new Error('Output directory does not exist and could not be created: ' + o.path)
+      }
+    }
+
+    const pathMd = path.join(o.path, id + '.md')
+    const pathHtml = path.join(o.path, id + '.html')
+    const pathPdf = path.join(o.path, id + '.pdf')
+
+    // write md
+    if (o.writeMarkdown) {
+      let err
+      writeFile(pathMd, markdown)
+      .then(() => console.log(`✨  ${id}.md done`))
+      .catch(e => { err = e })
+      if (err) throw err
+    }
+
+    // write html
+    const html = await ghmd(id, markdown, o.template, o.ghmd)
+    await writeFile(pathHtml, html)
+    console.log(`✨  ${id}.html done`)
+
+    // wait for print existing tasks
+    await Promise.all(this._printJobs)
+
+    // write pdf
+    this._page = await this._browser.newPage()
+    this._printJobs.push(this._pdf(pathHtml, pathPdf, o.pdf))
+    await Promise.all(this._printJobs)
+
+    console.log(`✨  ${id}.pdf done`)
+  } catch (e) {
+    console.error(`🚨  Error printing ${id} !`)
+    throw e
+  }
+
+  return this
+}
+
+Press.prototype._pdf = async function (file, out, opts) {
+  try {
+    await this._page.goto('file://' + file, {
+      waitUntil: 'networkidle'
+    })
+    await this._page.pdf(extend(opts, { path: out }))
+    return out
+  } catch (e) {
+    throw e
+  }
 }
--- test.js
+++ test.js
@@ -4,43 +4,127 @@ const fs = require('fs')
 const path = require('path')
 const rimraf = require('rimraf')
 const test = require('tape')
-const press = require('.')
+const letterpress = require('.')
 const letter = require('./test.md')
 
 const data = JSON.parse(fs.readFileSync(path.join(__dirname, 'test.json')))
 const date = new Date()
 const dateString = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
 
-test('press test', function (t) {
-  const dist = path.resolve('test')
-  const paths = ['dapibus_bar.html', 'dapibus_foo.md', 'parturient_bar.pdf', 'dapibus_bar.md', 'dapibus_foo.pdf', 'parturient_foo.html', 'dapibus_bar.pdf', 'parturient_bar.html', 'parturient_foo.md', 'dapibus_foo.html', 'parturient_bar.md', 'parturient_foo.pdf']
+test('letterpress test single auto', function (t) {
+  const out = path.join(__dirname, 'test')
+  const expected = ['dapibus_foo.md', 'dapibus_foo.html', 'dapibus_foo.pdf']
 
-  t.plan(paths.length)
-  t.comment('removing dir ' + dist)
-  rimraf(dist, function (err) {
-    if (err) {
-      t.fail(err.toString())
-      return
-    }
-    const proms = run({
-      dist: dist,
-      quiet: true
+  t.plan(expected.length + 1)
+
+  const markdown = letter(dateString, data.from[0], data.to[0])
+  const id = data.from[0].id + '_' + data.to[0].id
+
+  t.comment(`removing dir ${out} ...`)
+  rimraf(out, function (err) {
+    if (err) throw err
+
+    letterpress.print(id, markdown, { path: out })
+    .catch(e => {
+      console.error(e.toString())
+      t.fail('complete print')
     })
-    Promise.all(proms).then(() => {
-      paths.forEach(p => {
-        t.ok(fs.existsSync(path.join(dist, p)), 'file exists: ' + p)
-      })
+    .then(() => {
+      t.pass('complete print')
+      expected.forEach(p => t.ok(fs.existsSync(path.join(out, p)), 'exists: ' + p))
       t.end()
-    }).catch(e => t.fail(e.toString()))
+    })
   })
 })
 
-function run (opts) {
-  const proms = []
-  data.from.forEach(from => data.to.forEach(to => {
-    const md = letter(dateString, from, to)
-    const id = from.id + '_' + to.id
-    proms.push(press(id, md, opts))
-  }))
-  return proms
-}
+test('letterpress test single with constructor', function (t) {
+  const out = path.join(__dirname, 'test')
+  const expected = ['dapibus_foo.md', 'dapibus_foo.html', 'dapibus_foo.pdf']
+
+  t.plan(expected.length + 1)
+
+  const markdown = letter(dateString, data.from[0], data.to[0])
+  const id = data.from[0].id + '_' + data.to[0].id
+
+  t.comment(`removing dir ${out} ...`)
+  rimraf(out, async function (err) {
+    if (err) throw err
+
+    let press
+    try {
+      press = await letterpress.launch({ path: out })
+      await press.print(id, markdown)
+      press.close()
+      t.pass('complete print')
+    } catch (e) {
+      console.error(e.toString())
+      t.fail('complete print')
+      if (press && press.close) {
+        press.close()
+      }
+    }
+
+    expected.forEach(p => t.ok(fs.existsSync(path.join(out, p)), 'exists: ' + p))
+    t.end()
+  })
+})
+
+test('letterpress test multiple asynchronous', function (t) {
+  const out = path.join(__dirname, 'test')
+  const expected = [
+    'dapibus_foo.html',
+    'dapibus_bar.html',
+    'parturient_foo.html',
+    'parturient_bar.html',
+    'dapibus_foo.pdf',
+    'dapibus_bar.pdf',
+    'parturient_foo.pdf',
+    'parturient_bar.pdf'
+  ]
+
+  t.plan(expected.length + (data.from.length * data.to.length) + 1)
+
+  t.comment(`removing dir ${out} ...`)
+  rimraf(out, async function (err) {
+    if (err) throw err
+
+    let press
+    try {
+      press = await letterpress.launch({
+        path: out,
+        writeMarkdown: false
+      })
+
+      const ps = [] // promises
+      data.from.forEach(from => {
+        data.to.forEach(to => {
+          const markdown = letter(dateString, from, to)
+          const id = from.id + '_' + to.id
+
+          ps.push(
+            press.print(id, markdown)
+            .then(() => t.pass('print ' + id))
+            .catch(e => {
+              console.error(e.toString())
+              t.fail('print ' + id)
+            })
+          )
+        })
+      })
+
+      await Promise.all(ps)
+
+      press.close()
+      t.pass('complete print multiple')
+    } catch (e) {
+      console.error(e.toString())
+      t.fail('complete print multiple')
+      if (press && press.close) {
+        press.close()
+      }
+    }
+
+    expected.forEach(p => t.ok(fs.existsSync(path.join(out, p)), 'exists: ' + p))
+    t.end()
+  })
+})
