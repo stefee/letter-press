7ecb5988c79f892d83bf9a4521f67ca4ea92bb0a
Ari Lerner <arilerner@mac.com>
2018-01-24 13:56:01 -0500

    Fixed lint errors on ghmd.js

--- ghmd.js
+++ ghmd.js
@@ -1,14 +1,14 @@
 // based on 1000ch/node-github-markdown
 // https://github.com/1000ch/node-github-markdown
-const fs = require('fs');
-const path = require('path');
-const extend = require('xtend');
-const pug = require('pug');
-const MarkdownIt = require('markdown-it');
-const hljs = require('highlight.js');
+const fs = require('fs')
+const path = require('path')
+const extend = require('xtend')
+const pug = require('pug')
+const MarkdownIt = require('markdown-it')
+const hljs = require('highlight.js')
 
 module.exports = (title, markdown, opts) => {
-  opts = opts || {};
+  opts = opts || {}
 
   return new Promise((resolve, reject) => {
     try {
@@ -20,34 +20,34 @@ module.exports = (title, markdown, opts) => {
             langPrefix: 'hljs ',
             highlight: (string, lang) => {
               try {
-                if (lang) return hljs.highlight(lang, string).value;
-                return hljs.highlightAuto(string).value;
+                if (lang) return hljs.highlight(lang, string).value
+                return hljs.highlightAuto(string).value
               } catch (err) {
-                reject(err);
+                reject(err)
               }
             }
           },
           opts.markdown || {}
         )
-      );
+      )
 
       if (opts.markdownItPlugins) {
         opts.markdownItPlugins.forEach(plugin => {
           if (Array.isArray(plugin)) {
-            markdownIt.use(...plugin);
+            markdownIt.use(...plugin)
           } else {
-            markdownIt.use(plugin);
+            markdownIt.use(plugin)
           }
-        });
+        })
       }
 
-      let baseDir;
+      let baseDir
       if (opts.template) {
-        baseDir = path.dirname(opts.template);
+        baseDir = path.dirname(opts.template)
       } else {
         baseDir = fs.existsSync(path.join(__dirname, 'node_modules'))
           ? __dirname
-          : path.join(__dirname, '../..');
+          : path.join(__dirname, '../..')
       }
       const pugOpts = extend(
         {
@@ -57,14 +57,14 @@ module.exports = (title, markdown, opts) => {
           content: markdownIt.render(markdown)
         },
         opts.pug || {}
-      );
+      )
       const file = pug.renderFile(
         path.resolve(opts.template || path.join(__dirname, 'ghmd.pug')),
         pugOpts
-      );
-      resolve(file);
+      )
+      resolve(file)
     } catch (e) {
-      reject(e);
+      reject(e)
     }
-  });
-};
+  })
+}
