4e8b86aaf51f0eb795976d1530b3325d22137e91
Stef J <git@srilq.email>
2018-01-26 23:37:21 +0000

    [squash] inline toc options

--- toc.js
+++ toc.js
@@ -2,12 +2,9 @@ const htmlToc = require('html-toc')
 const extend = require('xtend')
 
 module.exports = async function toc (html, opts) {
-  opts = extend({
+  return htmlToc(html, extend({
     id: '#toc',
     anchors: true,
     selectors: 'h1,h2'
-  }, opts || {})
-
-  const content = htmlToc(html, opts)
-  return content
+  }, opts || {}))
 }
